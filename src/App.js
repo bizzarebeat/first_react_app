b003b69bec783832ac0cb037051ad918a4eb43ff
Jaroslav <graph2k@mail.ru>
2024-04-08 10:04:17 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {ToastContainer, toast} from 'react-toastify';
 
 const App = () => {
-  return (
+
+  const handleClick = () => {
+    toast.success("Hello from reactjs")
+  }
+  return (<>
+    <ToastContainer />
     <div>App</div>
-  )
+    <button onClick={handleClick} className='btn btn-warning btn-lg'>Click Me</button>
+    </>)
 }
 
 export default App
